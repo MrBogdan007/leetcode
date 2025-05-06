32f29c990710dbae82281f266d09f38ae89a789d
Bohdan Brokhovetskiy <bogdanbrohovetsky@gmail.com>
2025-05-06 18:00:02 +0300

    leetcode commit at this day and time: 2025-05-06 18:000

--- leetcode/src/App.java
+++ leetcode/src/App.java
@@ -1,6 +1,9 @@
+import java.util.HashMap;
+
 public class App {
     public static void main(String[] args) throws Exception {
         twoSum(new int []{2,7,11,15}, 9);
+        twoSumHashMap(new int []{2,7,11,15}, 9);
     }
 
     public static int[] twoSum(int[] nums, int target) {
@@ -15,5 +18,19 @@ public class App {
         }
        }
        return null;
+    }
+    public static int[] twoSumHashMap(int[] nums, int target) {
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i < nums.length; i++){
+            int complement = target - nums[i] ;
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return new int[]{};
     }
 }
