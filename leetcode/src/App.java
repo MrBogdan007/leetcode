8ac2ec31bd6a479244b88e96364ae4b9ba77bb94
Bohdan Brokhovetskiy <bogdanbrohovetsky@gmail.com>
2025-05-22 23:00:02 +0300

    leetcode commit at this day and time: 2025-05-22 23:000

--- leetcode/src/App.java
+++ leetcode/src/App.java
@@ -8,7 +8,9 @@ public class App {
         TwoSum.twoSumHashMap(new int []{2,7,11,15}, 9);
         removeDuplicatesJudge(new int[]{1,1,2});
         RemoveElements.removeElement(new int []{3,2,2,3}, 3);
-
+        PlusOne.plusOne(new int []{9});
+//        PlusOne.plusOne(new int []{9,9});
+        PlusOne.plusOne(new int []{8,9,9,9});
 
     }
 
