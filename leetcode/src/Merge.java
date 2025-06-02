dcb6a88b3de233ebad9f5314d3104f9361e71e70
Bohdan Brokhovetskiy <bogdanbrohovetsky@gmail.com>
2025-06-02 21:00:01 +0300

    leetcode commit at this day and time: 2025-06-02 21:000

---
+++ leetcode/src/Merge.java
@@ -0,0 +1,10 @@
+public class Merge {
+
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int nums1Length = m - 1;
+            int num2Length = n - 1;
+            int bothLength = m + n - 1;
+
+        }
+
+}
