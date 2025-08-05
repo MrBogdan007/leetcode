0293889fd54e256c1f2a02890d0e2f0202f4d1d8
Bohdan <bogdanbrohovetsky@gmail.com>
2025-08-05 21:17:19 +0300

    added multi dimentional dashes

--- src/arrays/LongestCommonPrefix.java
+++ src/arrays/LongestCommonPrefix.java
@@ -4,13 +4,10 @@ import java.util.Arrays;
 
 public class LongestCommonPrefix {
     public static void main(String[] args) {
-        String[] strs = new String[]{"flower","flow","flight"};
+        String[] strs = new String[]{"flower","ahw","fliaght"};
         System.out.println(longestPrefix(strs));
     }
     public static String longestPrefix(String[] strs){
-
-        String charString = "Banana";
-        System.out.println(charString.charAt(2));
         Arrays.sort(strs);
         String s1 = strs[0];
         String s2 = strs[strs.length-1];
@@ -24,5 +21,7 @@ public class LongestCommonPrefix {
         }
         return s1.substring(0, idx);
     }
+
+
 
 }
---
+++ src/w3resource/array/Ex3.java
@@ -0,0 +1,16 @@
+package w3resource.array;
+
+import java.util.Arrays;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        int [][] mult = new int [10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.printf("-");
+            }
+            System.out.println();
+        }
+
+    }
+}
